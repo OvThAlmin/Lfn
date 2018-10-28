c8edfedd37d0aa855249e4bce62bd53579e6a22d
OvThAlmin <text@example.com>
2018-10-29 00:31:48 +0900

    write skill

--- components/Services/ServiceBox.vue
+++ components/Services/ServiceBox.vue
@@ -15,7 +15,7 @@
           <div class="box">
             <div class="content">
               <h4 class="title is-5">Back End Web Development</h4>
-              Develop Back End application/service using Node.js or ASP .NET and SQL server or Mongo DB databases.
+              Develop Back End application/service using Spring Framework by Java 8.
             </div>
           </div>
         </div>
@@ -25,17 +25,17 @@
         <div class="column">
           <div class="box">
             <div class="content">
-              <h4 class="title is-5">Front End Web Development</h4>
-              Develop Front End using latest standards with HTML5/CSS3 with added funtionality using JavaScript and
-              Vue.js.
+              <h4 class="title is-5">Database</h4>
+              Develop and Design MySQL by Amazon Aurora and Cassandra databases.
             </div>
           </div>
         </div>
         <div class="column">
           <div class="box">
             <div class="content">
-              <h4 class="title is-5">Back End Web Development</h4>
-              Develop Back End application/service using Node.js or ASP .NET and SQL server or Mongo DB databases.
+              <h4 class="title is-5">Infrastructure</h4>
+              Migrate Cassandra to MySQL by Aurora.<br>
+              Develop This page by Netlify.
             </div>
           </div>
         </div>
