3103abf13addd70e29d6c66965a5e833ff3c56f0
OvThAlmin <text@example.com>
2018-10-31 23:48:18 +0900

    replace v-for in service

--- components/Services/ServiceBox.vue
+++ components/Services/ServiceBox.vue
@@ -2,40 +2,11 @@
    <!-- Services -->
     <div class="container">
       <div class="columns">
-        <div class="column">
+       <div class="column"  v-for="skill in skillSet.data" :key="skill.id">
           <div class="box">
             <div class="content">
-              <h4 class="title is-5">Front End Web Development</h4>
-              Develop Front End using latest standards with HTML5/CSS3 with added funtionality using JavaScript and
-              Vue.js.
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          <div class="box">
-            <div class="content">
-              <h4 class="title is-5">Back End Web Development</h4>
-              Develop Back End application/service using Spring Framework by Java 8.
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="columns">
-        <div class="column">
-          <div class="box">
-            <div class="content">
-              <h4 class="title is-5">Database</h4>
-              Develop and Design MySQL by Amazon Aurora and Cassandra databases.
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          <div class="box">
-            <div class="content">
-              <h4 class="title is-5">Infrastructure</h4>
-              Migrate Cassandra to MySQL by Aurora.<br>
-              Develop This page by Netlify.
+              <h4 class="title is-5">{{ skill.title }}</h4>
+              {{ skill.detail }}
             </div>
           </div>
         </div>
@@ -43,5 +14,37 @@
     </div>
 </template>
 
-<style>
-</style>
+<script>
+export default {
+  data() {
+    return {
+      database: 'demo',
+      skillSet: {
+        data: [
+          {
+            title: 'Infrastructure',
+            detail:
+              'Migrate Cassandra to MySQL by Aurora. Develop This page by Netlify.'
+          },
+          {
+            title: 'Back End Web Development',
+            detail:
+              'Develop Back End application/service using Spring Framework by Java 8.'
+          },
+          {
+            title: 'Database',
+            detail:
+              'Develop and Design MySQL by Amazon Aurora and Cassandra databases.'
+          },
+          {
+            title: 'Front End Web Development',
+            detail:
+              'Develop Front End using latest standards with HTML5/CSS3 with added funtionality using JavaScript and Vue.js.'
+          }
+        ],
+        name: ''
+      }
+    }
+  }
+}
+</script>
