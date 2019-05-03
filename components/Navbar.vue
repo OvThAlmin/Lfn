ce9abc69cef8a857520c7b2b0cef95827cedce8e
OvThAlmin <text@example.com>
2019-05-03 16:10:03 +0900

    refs rm about page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,18 +2,24 @@
   <nav class="navbar is-white" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <div class="navbar-item">Lfn</div>
-      <div class="navbar-burger" data-target="navMenu" @click="$store.commit('toggleMenu')" :class="{'is-active': $store.state.isMenuActive}">
+      <div
+        class="navbar-burger"
+        data-target="navMenu"
+        @click="$store.commit('toggleMenu')"
+        :class="{'is-active': $store.state.isMenuActive}"
+      >
         <span></span>
         <span></span>
         <span></span>
       </div>
-    </div><!-- navbar-brand END -->
+    </div>
+    <!-- navbar-brand END -->
     <div class="navbar-menu" id="navMenu" :class="{'is-active': $store.state.isMenuActive}">
       <div class="navbar-end">
         <nuxt-link to="/" class="navbar-item">トップ</nuxt-link>
-        <nuxt-link to="/about" class="navbar-item">About me</nuxt-link>
         <nuxt-link to="/contact" class="navbar-item">お問い合わせ</nuxt-link>
       </div>
-    </div><!-- navbar-menu END -->
+    </div>
+    <!-- navbar-menu END -->
   </nav>
 </template>
