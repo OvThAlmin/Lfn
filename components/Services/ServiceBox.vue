ad0b3382edac8815afaf349479ce0d84abf40557
OvThAlmin <text@example.com>
2019-05-03 16:34:45 +0900

    refs fix text

--- components/Services/ServiceBox.vue
+++ components/Services/ServiceBox.vue
@@ -1,49 +1,49 @@
 <template>
-   <!-- Services -->
-    <div class="container">
-      <div class="columns">
-       <div class="column"  v-for="skill in skillSet.data" :key="skill.id">
-          <div class="box">
-            <div class="content">
-              <h4 class="title is-5">{{ skill.title }}</h4>
-              {{ skill.detail }}
-            </div>
+  <!-- Services -->
+  <div class="container">
+    <div class="columns">
+      <div class="column" v-for="skill in skillSet.data" :key="skill.id">
+        <div class="box">
+          <div class="content">
+            <h4 class="title is-5">{{ skill.title }}</h4>
+            {{ skill.detail }}
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      database: 'demo',
+      database: "demo",
       skillSet: {
         data: [
           {
-            title: 'Front End Web Development',
+            title: "Front End Web Development",
             detail:
-              'Develop Front End using latest standards with HTML5/CSS3 with added funtionality using Angular and Nuxt.js.'
+              "Develop Front End using TypeScript with added funtionality using Angular and Nuxt.js."
           },
           {
-            title: 'Back End Web Development',
+            title: "Back End Web Development",
             detail:
-              'Develop Back End application/service using Spring Framework by Java 8.'
+              "Develop Back End application/service using Spring Framework by Java 8."
           },
           {
-            title: 'Database',
+            title: "Database",
             detail:
-              'Develop and Design MySQL in Amazon Aurora and Cassandra databases.'
+              "Develop and Design MySQL in Amazon Aurora and Cassandra databases."
           },
           {
-            title: 'Infrastructure',
-            detail: 'Build and Deploy This page by Netlify.'
+            title: "Infrastructure",
+            detail: "Build and Deploy This page by Netlify."
           }
         ],
-        name: ''
+        name: ""
       }
-    }
+    };
   }
-}
+};
 </script>
