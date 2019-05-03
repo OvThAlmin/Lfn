6253489b9f357df61c015e48fd6713ab03f8843a
OvThAlmin <text@example.com>
2019-05-03 15:32:42 +0900

    refs remove sample page

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,35 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        Lfn
+<div>
+  <section class="hero is-primary is-bold">
+    <div class="hero-body">
+      <h1 class="title is-size-2">
+        About
       </h1>
-      <h2 class="subtitle">
-        My super-excellent Nuxt.js project
+      <h2 class="subtitle is-size-4">
+        My Portfolio
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
+  
+  <aboutme/>
+  <services/>
+  <contact/>
+</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Aboutme from '~/components/Aboutme.vue'
+import Services from '~/components/Services.vue'
+import Contact from '~/components/Contact.vue'
 
 export default {
-  components: {
-    Logo
-  },
   fetch({ store }) {
     store.commit('resetMenu')
+  },
+  components: {
+    Aboutme,
+    Services,
+    Contact
   }
 }
 </script>
-
-<style>
-/* .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-} */
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
