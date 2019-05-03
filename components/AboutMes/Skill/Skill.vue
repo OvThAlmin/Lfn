80d433cc8ab3634433fb7dd6403dea15d84377ef
OvThAlmin <text@example.com>
2019-05-03 16:15:56 +0900

    refs fix skill

--- components/AboutMes/Skill/Skill.vue
+++ components/AboutMes/Skill/Skill.vue
@@ -1,66 +1,64 @@
 <template>
-      <div class="column">
-        <!-- Skills -->
-        <div class="card">
-          <div class="card-content skills-content">
-            <h3 class="title is-4">Skills</h3>
-            <div class="content">
-
-              <article class="media" v-for="skill in skillSet.data" :key="skill.id">
-                <div class="media-content">
-                  <div class="content">
-                    <p>
-                      <strong>{{ skill.title }}</strong>
-                      <br>
-                      <progress class="progress is-primary" :value="skill.level" max="100"></progress>
-                    </p>
-                  </div>
-                </div>
-              </article>
-
+  <div class="column">
+    <!-- Skills -->
+    <div class="card">
+      <div class="card-content skills-content">
+        <h3 class="title is-4">Skills</h3>
+        <div class="content">
+          <article class="media" v-for="skill in skillSet.data" :key="skill.id">
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>{{ skill.title }}</strong>
+                  <br>
+                  <progress class="progress is-primary" :value="skill.level" max="100"></progress>
+                </p>
+              </div>
             </div>
-          </div>
+          </article>
         </div>
       </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      database: 'demo',
+      database: "demo",
       skillSet: {
         data: [
           {
-            title: 'JavaScript:',
+            title: "TypeScript:",
             level: 70
           },
           {
-            title: 'Angular:',
-            level: 55
+            title: "Angular:",
+            level: 70
           },
           {
-            title: 'Nuxt.js:',
+            title: "Nuxt.js:",
             level: 40
           },
           {
-            title: 'Java/Spring:',
+            title: "Java/Spring:",
             level: 100
           },
           {
-            title: 'Databases -MySQL',
+            title: "Databases -MySQL",
             level: 95
           },
           {
-            title: 'Databases -NoSQL',
-            level: 80
+            title: "Databases -NoSQL",
+            level: 60
           }
         ],
-        name: ''
+        name: ""
       }
-    }
+    };
   }
-}
+};
 </script>
 
 
