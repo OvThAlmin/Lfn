9673a4133cd248d84f4cc7b0cc1f34606e5a705b
OvThAlmin <text@example.com>
2019-05-03 16:17:35 +0900

    refs add atcoder link

--- components/AboutMes/Skill/Profile.vue
+++ components/AboutMes/Skill/Profile.vue
@@ -1,37 +1,38 @@
 <template>
-      <div class="column">
-        <!-- Profile -->
-        <div class="card">
-          <div class="card-content">
-            <h3 class="title is-4">Profile</h3>
+  <div class="column">
+    <!-- Profile -->
+    <div class="card">
+      <div class="card-content">
+        <h3 class="title is-4">Profile</h3>
 
-            <div class="content">
-              <table class="table-profile">
-                <tr>
-                  <th colspan="1"></th>
-                  <th colspan="2"></th>
-                </tr>
-                <tr>
-                  <td>Address:</td>
-                  <td>Tokyo, Japan</td>
-                </tr>
-                <tr>
-                  <td>Name:</td>
-                  <td>OvThAlmin</td>
-                </tr>
-                <tr>
-                  <td>Email:</td>
-                  <td>minion@despicable.me</td>
-                </tr>
-              </table>
-            </div>
-            <br>
-            <div class="buttons has-addons is-centered">
-              <a href="https://github.com/OvThAlmin" class="button is-link">Github</a>
-              <a href="https://qiita.com/OvThAlmin" class="button is-link">Qiita</a>
-            </div>
-          </div>
+        <div class="content">
+          <table class="table-profile">
+            <tr>
+              <th colspan="1"></th>
+              <th colspan="2"></th>
+            </tr>
+            <tr>
+              <td>Address:</td>
+              <td>Tokyo, Japan</td>
+            </tr>
+            <tr>
+              <td>Name:</td>
+              <td>OvThAlmin</td>
+            </tr>
+            <tr>
+              <td>Email:</td>
+              <td>minion@despicable.me</td>
+            </tr>
+          </table>
+        </div>
+        <br>
+        <div class="buttons has-addons is-centered">
+          <a href="https://github.com/OvThAlmin" class="button is-link">Github</a>
+          <a href="https://qiita.com/OvThAlmin" class="button is-link">Qiita</a>
+          <a href="https://atcoder.jp/users/OvThAlmin" class="button is-link">AtCoder</a>
         </div>
       </div>
+    </div>
+  </div>
 </template>
 
